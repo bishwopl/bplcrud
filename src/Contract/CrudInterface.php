b6c153c0208b800465d604a536ea561d9008c7f9
bishwopl <bishwo.prasad@gmail.com>
2021-03-29 17:29:11 +0545

    Added method comments

--- src/Contract/CrudInterface.php
+++ src/Contract/CrudInterface.php
@@ -10,11 +10,31 @@ use BplCrud\QueryFilter;
 
 interface CrudInterface {
 
-    public function create();
+    /**
+     * Save $object in storage
+     * @param type $object
+     */
+    public function create($object);
 
-    public function read(QueryFilter $queryFilter, $offset = 0, $limit = 10);
+    /**
+     * Update record 
+     * @param type $object
+     */
+    public function update($object);
 
-    public function update();
-
+    /**
+     * Delete $object from storage
+     * @param type $object
+     */
     public function delete($object);
+
+    /**
+     * Read records from storage
+     * 
+     * @param \BplCrud\QueryFilter | array $queryFilter
+     * @param int $offset default 0
+     * @param int $limit default 10
+     * @param array type $orderBy Order by is of type ["columnName1"=>"ASC/DESC", "columnName2"=>"ASC/DESC"]
+     */
+    public function read($queryFilter, $offset = 0, $limit = 10, $orderBy = []);
 }
