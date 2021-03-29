959ad72af096ca33135334a9f4d8b608c7871606
bishwopl <bishwo.prasad@gmail.com>
2021-03-29 17:26:46 +0545

    Update Example

--- src/Generator/ServiceCreator.php
+++ src/Generator/ServiceCreator.php
@@ -10,7 +10,7 @@ use BplCrud\Crud;
 use Laminas\Code\Generator\ClassGenerator;
 use Laminas\Code\Generator\MethodGenerator;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\ClassMetadataFactory;;
+use Doctrine\ORM\Mapping\ClassMetadataFactory;
 
 class ServiceCreator {
 
@@ -58,14 +58,16 @@ class ServiceCreator {
             $dummy = explode('\\', $fcqn);
             $entityName = $dummy[sizeof($dummy) - 1];
 
-            $body = '$form = new ' . $entityName . 'Form($persistanceManager, "' . lcfirst($entityName) . '");' . PHP_EOL
-                    . 'parent::__construct($persistanceManager, $form, ' . $entityName . '::class);';
+            $body = '$form = new ' . $entityName . 'Form($persistanceManager, "' . lcfirst($entityName) . '");'. PHP_EOL
+                    . '$mapper = new \BplCrud\Mapper\DoctrineMapper($persistanceManager, '.$entityName.'::class);' . PHP_EOL
+                    . 'parent::__construct($mapper, $form, ' . $entityName . '::class);';
 
             $serviceGenerator = new ClassGenerator(
                     $entityName . 'Service', $this->serviceNamespace, null, Crud::class, [], [], [], null
             );
             $serviceGenerator->addUse($this->formNamespace . '\\' . $entityName . '\\' . $entityName . 'Form');
             $serviceGenerator->addUse($this->entityNamespace . '\\' . $entityName);
+            
             $method = new MethodGenerator(
                     "__construct", [['name' => 'persistanceManager', 'type' => EntityManagerInterface::class]], MethodGenerator::FLAG_PUBLIC, $body
             );
